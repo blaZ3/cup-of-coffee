fb97475e7be2917478b25e780b7dd0e13e5bbd36
blaZ3 <tellvivk@gmail.com>
2021-08-11 06:25:46 +0300

    Added SideDrawerTests

--- app/src/androidTest/java/com/example/cupofcoffee/views/SideDrawerTests.kt
+++ app/src/androidTest/java/com/example/cupofcoffee/views/SideDrawerTests.kt
@@ -1,4 +1,68 @@
 package com.example.cupofcoffee.views
 
+import androidx.compose.material.DrawerValue
+import androidx.compose.material.Text
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createComposeRule
+import com.example.cupofcoffee.app.composables.SideDrawer
+import com.example.cupofcoffee.app.data.models.SubReddit
+import com.example.cupofcoffee.app.data.models.defaultSubs
+import org.junit.Rule
+import org.junit.Test
+
 class SideDrawerTests {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    private val subs = listOf(
+        SubReddit("Sub1"),
+        SubReddit("Sub2"),
+        SubReddit("Sub3"),
+        SubReddit("Sub4"),
+    )
+
+    @Test
+    fun testSubsAreShown() {
+        composeTestRule.setContent {
+            SideDrawer(
+                startState = DrawerValue.Open,
+                selectedSubReddit = subs.first(),
+                subReddits = subs,
+                onChangeSubReddit = {},
+                onAddNewSubReddit = { },
+                onRemoveSubReddit = {}
+            ) {
+                Text(text = "This is test content")
+            }
+        }
+
+        subs.forEach { sub ->
+            composeTestRule.onNodeWithText("r/${sub.name}").assertIsDisplayed()
+            composeTestRule.onNodeWithText("r/${sub.name}").assertIsEnabled()
+        }
+    }
+
+
+    @Test
+    fun testRemoveSubIconIsShowForAllNonDefaultSubs() {
+        composeTestRule.setContent {
+            SideDrawer(
+                startState = DrawerValue.Open,
+                selectedSubReddit = subs.first(),
+                subReddits = defaultSubs.toMutableList().also { it.addAll(subs) },
+                onChangeSubReddit = {},
+                onAddNewSubReddit = { },
+                onRemoveSubReddit = {}
+            ) {
+                Text(text = "This is test content")
+            }
+        }
+
+        defaultSubs.forEach { sub ->
+            composeTestRule.onNodeWithTag("remove-sub-${sub.name}").assertDoesNotExist()
+        }
+        subs.forEach { sub ->
+            composeTestRule.onNodeWithTag("remove-sub-${sub.name}").assertIsDisplayed()
+        }
+    }
 }
--- app/src/main/java/com/example/cupofcoffee/app/composables/SideDrawer.kt
+++ app/src/main/java/com/example/cupofcoffee/app/composables/SideDrawer.kt
@@ -15,6 +15,8 @@ import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.testTag
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -90,12 +92,16 @@ fun SideDrawer(
                         Row(modifier = Modifier.weight(0.1f)) {
                             if (!defaultSubNames.contains(subReddit.name)) {
                                 Icon(
-                                    modifier = Modifier.clickable(true) {
-                                        scope.launch {
-                                            drawerState.close()
-                                            onRemoveSubReddit(subReddit.name)
+                                    modifier = Modifier
+                                        .semantics {
+                                            this.testTag = "remove-sub-${subReddit.name}"
                                         }
-                                    },
+                                        .clickable(true) {
+                                            scope.launch {
+                                                drawerState.close()
+                                                onRemoveSubReddit(subReddit.name)
+                                            }
+                                        },
                                     painter = painterResource(id = ic_remove),
                                     contentDescription = stringResource(remove_subreddit)
                                 )
